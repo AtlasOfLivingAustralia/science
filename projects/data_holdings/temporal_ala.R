f950d3bf96f776290a1558d6b6d0926249d56613
Dax Kellie <dax.kellie@csiro.au>
2021-05-14 12:39:49 +1000

    Add ala data

--- projects/data_holdings/temporal_ala.R
+++ projects/data_holdings/temporal_ala.R
@@ -178,15 +178,6 @@ p_day_of_week + p_month_of_year_dot + plot_layout(ncol=1,widths=c(2,1))
 day_interval <- seq(ymd("2012-01-01"), ymd("2020-12-31"), by = "days")
 
 
-# get some records
-records <- ala_occurrences(
-  taxa = select_taxa("Eolophus roseicapilla"),
-  filters = select_filters(cl22 = "Tasmania"),
-  columns = select_columns("verbatimEventDate", group = "basic"))
-# str(records)
-# unique(records$verbatimEventDate)
-## shows that minute-specific records are rare
-## ignore hourly stuff
 
 # set dates to date format
 occurrences_Tas$eventDate <- ymd(occurrences_Tas$eventDate)
@@ -256,7 +247,6 @@ date_df$residuals <- resid(model)
 
 
 # draw
-library(ggplot2)
 library(patchwork)
 
 a <- ggplot(prediction_1, aes(x = date_unscaled, y = fit)) +
@@ -279,6 +269,7 @@ c <- ggplot(date_df, aes(x = date, y = residuals)) +
 a / b / c
 
 
+# gc()
 
 # Adding lots and lots of data
 library(io)
@@ -287,3 +278,98 @@ files <- paste(data_dir, list_files(data_dir), sep = "/")
 data_list <- lapply(files, function(a){readRDS(a)})
 data_in <- data.table::rbindlist(data_list)
 
+
+
+# set time interval of interest
+day_interval <- seq(ymd("2012-01-01"), ymd("2020-12-31"), by = "days")
+
+
+
+# set dates to date format
+occurrences_Tas$eventDate <- ymd(occurrences_Tas$eventDate)
+
+# set up date dataset
+date_df <- data.frame(
+  date = day_interval,
+  date_numeric = as.numeric(day_interval),
+  date_julian = yday(day_interval)
+)
+
+# count records
+date_df$record_count <- unlist(lapply(
+  date_df$date,
+  function(a){
+    return(length(which(occurrences_Tas$eventDate == a)))
+  }))
+
+
+# model using GAMs
+# install.packages("mgcv")
+library(mgcv)
+date_df$date_scaled <- scale(date_df$date_numeric)
+date_df$julian_scaled <- scale(date_df$date_julian)
+
+
+# try a model with interacting date terms
+model <- gam(record_count ~ s(date_scaled, by = julian_scaled),
+             data = date_df,
+             family = poisson(link = "log"))
+
+# make predictions
+# first of change over time
+model <- gam(record_count ~ s(date_scaled) + s(julian_scaled),
+             data = date_df,
+             family = poisson(link = "log"))
+
+prediction_1 <- data.frame(
+  date_scaled = seq(
+    min(date_df$date_scaled),
+    max(date_df$date_scaled),
+    length.out = 100),
+  julian_scaled = 0)
+prediction_1$date_unscaled <- seq(min(date_df$date), max(date_df$date), length.out = 100) # readjusting scale for plotting
+
+model_prediction <- predict(model, newdata = prediction_1, se.fit = TRUE)
+prediction_1$fit <- exp(model_prediction$fit)
+prediction_1$lci <- exp(model_prediction$fit - (2 * model_prediction$se.fit))
+prediction_1$uci <- exp(model_prediction$fit + (2 * model_prediction$se.fit))
+
+# then repeat for Julian Date
+prediction_2 <- data.frame(
+  julian_scaled = seq(
+    min(date_df$julian_scaled),
+    max(date_df$julian_scaled),
+    length.out = 100),
+  date_scaled = 0)
+prediction_2$date_unscaled <- seq(min(date_df$date_julian), max(date_df$date_julian), length.out = 100)
+
+model_prediction <- predict(model, newdata = prediction_2, se.fit = TRUE)
+prediction_2$fit <- exp(model_prediction$fit)
+prediction_2$lci <- exp(model_prediction$fit - (2 * model_prediction$se.fit))
+prediction_2$uci <- exp(model_prediction$fit + (2 * model_prediction$se.fit))
+
+# finally calculate residuals from the model
+date_df$residuals <- resid(model)
+
+
+# draw
+library(patchwork)
+
+a <- ggplot(prediction_1, aes(x = date_unscaled, y = fit)) +
+  geom_ribbon(aes(ymin = lci, ymax = uci), fill = "#ff6e6e") +
+  geom_path() +
+  theme_bw() +
+  labs(x = "Year", y = "Number of Records")
+
+b <- ggplot(prediction_2, aes(x = date_unscaled, y = fit)) +
+  geom_ribbon(aes(ymin = lci, ymax = uci), fill = "#6e9eff") +
+  geom_path() +
+  theme_bw() + 
+  labs(x = "Day of year", y = "Number of Records")
+
+c <- ggplot(date_df, aes(x = date, y = residuals)) +
+  geom_path() +
+  # geom_point() +
+  theme_bw()
+
+a / b / c
