05d4696f57ff7b1623655f57e7eb1d5a59fe4967
Dax Kellie <dax.kellie@csiro.au>
2021-05-26 09:58:41 +1000

    Remove spline models
    * Shorten document to simple boxplots
    * Remove file information

--- projects/data_holdings/temporal_ala.R
+++ projects/data_holdings/temporal_ala.R
@@ -1,5 +1,5 @@
 ## ------------------------------------------------------#
-## Title: Temporal ALA Investigation
+## Title: General temporal trends of ALA
 ## Author: Dax Kellie
 ## Date Created: 2021-05-7
 ## ------------------------------------------------------#
@@ -29,7 +29,7 @@ find_field_values("basisOfRecord")
 # Get data
 
 # Might need to config email to get ALA data
-ala_config(email = "dax.kellie@csiro.au")
+# ala_config(email = )
 
 # How many occurrence records in Tasmania over last 10 years?
 ala_counts(filters = select_filters(stateProvince = "Tasmania",
@@ -42,29 +42,27 @@ ala_counts(filters = select_filters(stateProvince = "Tasmania",
 #                                                basisOfRecord = "HumanObservation"))
 
 # Call saved local data file
-path <- "C:/Users/KEL329/OneDrive - CSIRO/Documents/Projects/Data Holdings/data_ALA"
-data_filepath <- file.path(path, "tasmania_occurrences.rds")
-occurrences_Tas <- readRDS(file=data_filepath)
-# filename <- file.choose()
-# occurrences_Tas <- readRDS(filename)
+#   ./Data Holdings/data_ALA/eventDate/TAS_occurrences.rds"
+filename <- file.choose()
+occurrences_Tas <- readRDS(filename)
 
-# occurrences_Tas <- occurrences_Tas %>% filter(!is.na(eventDate)) # remove NA
+
 occurrences_Tas <- setDT(occurrences_Tas) # make data.table
 
 
 # add day, week
 occurrences_Tas$eventDate <- as_date(occurrences_Tas$eventDate)
-
-occurrences_Tas$weekday <- wday(occurrences_Tas$eventDate, label = TRUE) # day (sun,mon,tues, etc)
-occurrences_Tas$week <- week(occurrences_Tas$eventDate) # week
+occurrences_Tas$weekday <- wday(occurrences_Tas$eventDate, label = TRUE) # day (sun,mon,etc)
 occurrences_Tas$day <- yday(occurrences_Tas$eventDate) # day (julian)
+occurrences_Tas$week <- week(occurrences_Tas$eventDate) # week
 occurrences_Tas$month <- month(occurrences_Tas$eventDate, label = TRUE) # month
 occurrences_Tas$year <- year(occurrences_Tas$eventDate) # year
-?month()
+
 
 # remove NAs
 occurrences_Tas <- occurrences_Tas %>% filter(!is.na(eventDate)) %>% as.data.table() # remove NA
-any(is.na(occurrences_Tas$day))
+# any(is.na(occurrences_Tas$day))
+
 
 
 #------------------------------------------------------#
@@ -133,8 +131,9 @@ p_day_of_week
 
 # get month counts
 month_counts <- occurrences_Tas[, .N, by = c("month")]
-month_counts$month <- ordered(factor(month_counts$month, levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")))
-levels(month_counts$month)
+month_counts$month <- ordered(factor(month_counts$month, 
+                                     levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                                                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")))
 
 # Total number of occurrences on each day
 p_month_of_year <- month_counts %>% ggplot(aes(x = month, y = N)) + 
@@ -156,7 +155,7 @@ p_month_of_year_dot <- ggdotchart(month_counts, x = "month", y = "N",
                               sorting = "none", # Sort value in descending order
                               add = "segments", # Add segments from y = 0 to dots
                               legend = "none",
-                              rotate = TRUE,
+                              rotate = FALSE,
                               dot.size = 4,
                               add.params = list(color = "month", size = 1),
                               ylab = "Number of records",
@@ -165,216 +164,3 @@ p_month_of_year_dot <- ggdotchart(month_counts, x = "month", y = "N",
 )
 
 p_day_of_week + p_month_of_year_dot + plot_layout(ncol=1,widths=c(2,1))
-
-
-
-
-
-#----------------------------------------------#
-#                   spline
-#----------------------------------------------#
-
-# set time interval of interest
-day_interval <- seq(ymd("2012-01-01"), ymd("2020-12-31"), by = "days")
-
-
-
-# set dates to date format
-occurrences_Tas$eventDate <- ymd(occurrences_Tas$eventDate)
-
-# set up date dataset
-date_df <- data.frame(
-  date = day_interval,
-  date_numeric = as.numeric(day_interval),
-  date_julian = yday(day_interval)
-)
-
-# count records
-date_df$record_count <- unlist(lapply(
-  date_df$date,
-  function(a){
-    return(length(which(occurrences_Tas$eventDate == a)))
-  }))
-
-
-# model using GAMs
-# install.packages("mgcv")
-library(mgcv)
-date_df$date_scaled <- scale(date_df$date_numeric)
-date_df$julian_scaled <- scale(date_df$date_julian)
-
-
-# try a model with interacting date terms
-model <- gam(record_count ~ s(date_scaled, by = julian_scaled),
-             data = date_df,
-             family = poisson(link = "log"))
-
-# make predictions
-# first of change over time
-model <- gam(record_count ~ s(date_scaled) + s(julian_scaled),
-             data = date_df,
-             family = poisson(link = "log"))
-
-prediction_1 <- data.frame(
-  date_scaled = seq(
-    min(date_df$date_scaled),
-    max(date_df$date_scaled),
-    length.out = 100),
-  julian_scaled = 0)
-prediction_1$date_unscaled <- seq(min(date_df$date), max(date_df$date), length.out = 100) # readjusting scale for plotting
-
-model_prediction <- predict(model, newdata = prediction_1, se.fit = TRUE)
-prediction_1$fit <- exp(model_prediction$fit)
-prediction_1$lci <- exp(model_prediction$fit - (2 * model_prediction$se.fit))
-prediction_1$uci <- exp(model_prediction$fit + (2 * model_prediction$se.fit))
-
-# then repeat for Julian Date
-prediction_2 <- data.frame(
-  julian_scaled = seq(
-    min(date_df$julian_scaled),
-    max(date_df$julian_scaled),
-    length.out = 100),
-  date_scaled = 0)
-prediction_2$date_unscaled <- seq(min(date_df$date_julian), max(date_df$date_julian), length.out = 100)
-
-model_prediction <- predict(model, newdata = prediction_2, se.fit = TRUE)
-prediction_2$fit <- exp(model_prediction$fit)
-prediction_2$lci <- exp(model_prediction$fit - (2 * model_prediction$se.fit))
-prediction_2$uci <- exp(model_prediction$fit + (2 * model_prediction$se.fit))
-
-# finally calculate residuals from the model
-date_df$residuals <- resid(model)
-
-
-# draw
-library(patchwork)
-
-a <- ggplot(prediction_1, aes(x = date_unscaled, y = fit)) +
-  geom_ribbon(aes(ymin = lci, ymax = uci), fill = "#ff6e6e") +
-  geom_path() +
-  theme_bw() +
-  labs(x = "Year", y = "Number of Records")
-
-b <- ggplot(prediction_2, aes(x = date_unscaled, y = fit)) +
-  geom_ribbon(aes(ymin = lci, ymax = uci), fill = "#6e9eff") +
-  geom_path() +
-  theme_bw() + 
-  labs(x = "Day of year", y = "Number of Records")
-
-c <- ggplot(date_df, aes(x = date, y = residuals)) +
-  geom_path() +
-  # geom_point() +
-  theme_bw()
-
-a / b / c
-
-
-
-
-
-
-
-
-# WA
-
-
-
-# set time interval of interest
-day_interval <- seq(ymd("2012-01-01"), ymd("2020-12-31"), by = "days")
-
-
-
-# set dates to date format
-occurrences_WA$eventDate <- ymd(occurrences_WA$eventDate)
-
-# set up date dataset
-date_df <- data.frame(
-  date = day_interval,
-  date_numeric = as.numeric(day_interval),
-  date_julian = yday(day_interval)
-)
-
-# count records
-date_df$record_count <- unlist(lapply(
-  date_df$date,
-  function(a){
-    return(length(which(occurrences_WA$eventDate == a)))
-  }))
-
-
-# model using GAMs
-# install.packages("mgcv")
-library(mgcv)
-date_df$date_scaled <- scale(date_df$date_numeric)
-date_df$julian_scaled <- scale(date_df$date_julian)
-
-
-# try a model with interacting date terms
-model <- gam(record_count ~ s(date_scaled, by = julian_scaled),
-             data = date_df,
-             family = poisson(link = "log"))
-
-# make predictions
-# first of change over time
-model <- gam(record_count ~ s(date_scaled) + s(julian_scaled),
-             data = date_df,
-             family = poisson(link = "log"))
-
-prediction_1 <- data.frame(
-  date_scaled = seq(
-    min(date_df$date_scaled),
-    max(date_df$date_scaled),
-    length.out = 100),
-  julian_scaled = 0)
-prediction_1$date_unscaled <- seq(min(date_df$date), max(date_df$date), length.out = 100) # readjusting scale for plotting
-
-model_prediction <- predict(model, newdata = prediction_1, se.fit = TRUE)
-prediction_1$fit <- exp(model_prediction$fit)
-prediction_1$lci <- exp(model_prediction$fit - (2 * model_prediction$se.fit))
-prediction_1$uci <- exp(model_prediction$fit + (2 * model_prediction$se.fit))
-
-# then repeat for Julian Date
-prediction_2 <- data.frame(
-  julian_scaled = seq(
-    min(date_df$julian_scaled),
-    max(date_df$julian_scaled),
-    length.out = 100),
-  date_scaled = 0)
-prediction_2$date_unscaled <- seq(min(date_df$date_julian), max(date_df$date_julian), length.out = 100)
-
-model_prediction <- predict(model, newdata = prediction_2, se.fit = TRUE)
-prediction_2$fit <- exp(model_prediction$fit)
-prediction_2$lci <- exp(model_prediction$fit - (2 * model_prediction$se.fit))
-prediction_2$uci <- exp(model_prediction$fit + (2 * model_prediction$se.fit))
-
-# finally calculate residuals from the model
-date_df$residuals <- resid(model)
-
-
-# draw
-library(patchwork)
-
-a <- ggplot(prediction_1, aes(x = date_unscaled, y = fit)) +
-  geom_ribbon(aes(ymin = lci, ymax = uci), fill = "#ff6e6e") +
-  geom_path() +
-  theme_bw() +
-  labs(x = "Year", y = "Number of Records")
-
-b <- ggplot(prediction_2, aes(x = date_unscaled, y = fit)) +
-  geom_ribbon(aes(ymin = lci, ymax = uci), fill = "#6e9eff") +
-  geom_path() +
-  theme_bw() + 
-  labs(x = "Day of year", y = "Number of Records")
-
-c <- ggplot(date_df, aes(x = date, y = residuals)) +
-  geom_path() +
-  # geom_point() +
-  theme_bw()
-
-a / b / c
-
-a <- ggplot(prediction_1, aes(x = date_unscaled, y = fit)) +
-  geom_area(aes(ymin = lci, ymax = uci), fill = "#ff6e6e", alpha = 0.2) +
-  geom_path() +
-  theme_bw() +
-  labs(x = "Year", y = "Number of Records")
