a5c89c662ba888f13ad6c3162203494219e8e267
Martin Westgate <martinjwestgate@gmail.com>
2022-04-05 16:33:51 +1000

    add shiny/leaflet interactivity
    - leaflet now uses smaller version of shapefile, which is faster
    - clickable images are used to redraw map via leafletProxy
    - restricted to vertebrates for now, but option to add inverts via toggle

--- projects/shiny-election-app/2022_electorate_map/app.R
+++ projects/shiny-election-app/2022_electorate_map/app.R
@@ -4,9 +4,55 @@ library(htmltools)
 library(leaflet)
 library(dplyr)
 
-map_data <- readRDS("simpl_spatial_data.rds")
-map_tibble <- as_tibble(map_data)
-leaflet_map <- readRDS("leaflet_map.rds")
+# data for plotting
+species_data <- readRDS("common_names.rds")
+spatial_data <- readRDS("simpl_spatial_data.rds")
+
+# functions: park in .R folder later on
+leaflet_update <- function(df){
+  leafletProxy("mymap") |>
+    clearShapes() |>
+    addPolygons(
+      data = df,
+      layerId = df$Elect_div,
+      color = "#C44D34",
+      fillColor = "#C44D34",
+      fillOpacity = 0.6,
+      weight = 3,
+      label = lapply(df$label, HTML),
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", 
+                     padding = "3px 5px"),
+        textsize = "12px",
+        direction = "auto")
+    )
+}
+
+leaflet_reset <- function(){
+  leafletProxy("mymap") |>
+    clearShapes() |>
+    addPolygons(
+      data = spatial_data,
+      layerId = spatial_data$Elect_div,
+      fillColor = "white",
+      fillOpacity = .2, 
+      color = "#111111", 
+      weight = 1, 
+      stroke = TRUE,
+      highlightOptions = highlightOptions(
+        color = "#C44D34", 
+        fillColor = "#C44D34", 
+        fillOpacity = 0.6,
+        weight = 3,
+        bringToFront = TRUE),
+      label = lapply(spatial_data$label, HTML),
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", 
+                     padding = "3px 5px"),
+        textsize = "12px",
+        direction = "auto")
+    )
+}
 
 ui <- fluidPage(
   # import css
@@ -26,20 +72,56 @@ ui <- fluidPage(
         tableOutput("text"),
       ),
       # lower-right section: taxonomic groups
-      tags$div(class = "textdiv",
-        # code to place an image of a bird as a link
-        tags$div(class = "profile_div",
-          tags$a(
-            id = "taxon_birds",
-            href = "#",
-            class = "action-button shiny-bound-input",
-            tags$image(
-              class = "profile",
-              src="images/swift_parrot.jpeg"
-            )
-          ),
-          tags$p(class = "profile_text", "Birds")
+      tags$a(
+        id = "taxon_birds",
+        href = "#",
+        class = "action-button shiny-bound-input",
+        tags$image(
+          class = "profile",
+          src="images/swift_parrot.jpeg"
         )
+      ),
+      tags$a(
+        id = "taxon_mammals",
+        href = "#",
+        class = "action-button shiny-bound-input",
+        tags$image(
+          class = "profile",
+          src="images/swift_parrot.jpeg"
+        )
+      ),
+      tags$a(
+        id = "taxon_reptiles",
+        href = "#",
+        class = "action-button shiny-bound-input",
+        tags$image(
+          class = "profile",
+          src="images/swift_parrot.jpeg"
+        )
+      ),
+      tags$a(
+        id = "taxon_fish",
+        href = "#",
+        class = "action-button shiny-bound-input",
+        tags$image(
+          class = "profile",
+          src="images/swift_parrot.jpeg"
+        )
+      ),
+      tags$a(
+        id = "taxon_frogs",
+        href = "#",
+        class = "action-button shiny-bound-input",
+        tags$image(
+          class = "profile",
+          src="images/swift_parrot.jpeg"
+        )
+      ),
+      tags$a(
+        id = "taxon_clear",
+        href = "#",
+        class = "action-button shiny-bound-input",
+        tags$div(class = "clear_circle", "show all")
       )
     )
   )
@@ -52,28 +134,103 @@ server <- function(input, output){
     type = "none",
     taxon_name = c(),
     taxon_classification = c()
+    # group = "none"
   )
   
   # render leaflet map
-  output$mymap <- renderLeaflet(leaflet_map)
+  output$mymap <- renderLeaflet({
+    leaflet() |> 
+      fitBounds(112, -45, 154, -10) |>
+      addProviderTiles(providers$CartoDB.Positron) |>  
+      addPolygons(
+        data = spatial_data,
+        layerId = spatial_data$Elect_div,
+        fillColor = "white",
+        fillOpacity = .2, 
+        color = "#111111", 
+        weight = 1, 
+        stroke = TRUE,
+        highlightOptions = highlightOptions(
+          color = "#C44D34", 
+          fillColor = "#C44D34", 
+          fillOpacity = 0.6,
+          weight = 3,
+          bringToFront = TRUE),
+        label = lapply(spatial_data$label, HTML),
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", 
+                       padding = "3px 5px"),
+          textsize = "12px",
+          direction = "auto")
+      )
+  })
 
   # capture click events on map
   observeEvent(input$mymap_shape_click$id, { # previously `mouseover`
     user_data$type <- "map"
-    user_data$click <- map_tibble |> 
-      filter(Elect_div == input$mymap_shape_click$id)
+    user_data$click <- species_data |> 
+      filter(electorate == input$mymap_shape_click$id)
     # any way to 'click off' map to show intro text?
   })
+
   
   # capture click events on taxa
   observeEvent(input$taxon_birds, {
     user_data$type <- "taxon"
-    user_data$click <- map_tibble |> filter(class == "Aves")
+    user_data$click <- species_data |> filter(class == "Aves")
     user_data$taxon_name <- "Birds"
     user_data$taxon_classification <- "Chordata > Aves"
-    
-    # NEXT: use leafletProxy() to highligh all electorates with class == "Aves"
-    # https://rstudio.github.io/leaflet/shiny.html
+    map_tr <- spatial_data[
+      spatial_data$Elect_div %in% user_data$click$electorate, ]
+    leaflet_update(map_tr)
+  })
+  
+  observeEvent(input$taxon_mammals, {
+    user_data$type <- "taxon"
+    user_data$click <- species_data |> filter(class == "Mammalia")
+    user_data$taxon_name <- "Mammals"
+    user_data$taxon_classification <- "Chordata > Mammalia"
+    map_tr <- spatial_data[
+      spatial_data$Elect_div %in% user_data$click$electorate, ]
+    leaflet_update(map_tr)
+  })
+  
+  observeEvent(input$taxon_reptiles, {
+    user_data$type <- "taxon"
+    user_data$click <- species_data |> filter(class == "Reptilia")
+    user_data$taxon_name <- "Reptiles"
+    user_data$taxon_classification <- "Chordata > Reptilia"
+    map_tr <- spatial_data[
+      spatial_data$Elect_div %in% user_data$click$electorate, ]
+    leaflet_update(map_tr)
+  })
+  
+  observeEvent(input$taxon_frogs, {
+    user_data$type <- "taxon"
+    user_data$click <- species_data |> filter(class == "Amphibia")
+    user_data$taxon_name <- "Frogs"
+    user_data$taxon_classification <- "Chordata > Amphibia > Anura"
+    map_tr <- spatial_data[
+      spatial_data$Elect_div %in% user_data$click$electorate, ]
+    leaflet_update(map_tr)
+  })
+  
+  observeEvent(input$taxon_fish, {
+    user_data$type <- "taxon"
+    user_data$click <- species_data |> filter(class == "Actinopterygii" | class == "Chondrichthyes")
+    user_data$taxon_name <- "Fish"
+    user_data$taxon_classification <- "Classes Actinopterygii & Chondrichthyes"
+    map_tr <- spatial_data[
+      spatial_data$Elect_div %in% user_data$click$electorate, ]
+    leaflet_update(map_tr)
+  })
+  
+  observeEvent(input$taxon_clear, {
+    user_data$type <- "none"
+    user_data$click <- c()
+    user_data$taxon_name <- c()
+    user_data$taxon_classification <- c()
+    leaflet_reset()
   })
   
   # render text
@@ -87,13 +244,13 @@ server <- function(input, output){
     }else{
       if(user_data$type == "taxon"){
         bird_subset <- user_data$click[
-          order(user_data$click$z_squared, decreasing = TRUE), ]
+          order(user_data$click$z_sum, decreasing = TRUE), ]
         paste0(
           "<h1>", user_data$taxon_name, "</h1>",
           "<i>", user_data$taxon_classification, "</i>",
           "<h2>Most representative electorates</h2>",
           paste(
-            bird_subset$Elect_div[1:5], 
+            bird_subset$electorate[1:5], 
             bird_subset$vernacular_name[1:5],
             sep = " | ",
             collapse = "<br>")
@@ -102,7 +259,7 @@ server <- function(input, output){
       else if(user_data$type == "map"){
         paste0(
           "<h1>", 
-            user_data$click$Elect_div, ", ", user_data$click$state, 
+            user_data$click$electorate, ", ", user_data$click$state, 
           " </h1>",
           "<h1>", user_data$click$vernacular_name, "</h1>",
           "<h3><i>", user_data$click$species, "</i></h3>",
--- projects/shiny-election-app/election_prep.R
+++ projects/shiny-election-app/election_prep.R
@@ -8,6 +8,8 @@ library(galah)
 galah_config(email = "martinjwestgate@gmail.com")
 library(ggplot2)
 library(readr)
+library(leaflet)
+library(htmltools)
 
 # search_fields("elect") # most recent electorates are from 2021, not 2018 as in ALA
 
@@ -88,6 +90,32 @@ for(i in seq_along(in_list)){ # for each row
 # join into a single df
 species_df <- do.call(rbind, species_list)
 
+# calculate number of species per electorate
+electorate_df <- species_df |> 
+  group_by(electorate) |> 
+  summarize(electorate_spp = n_distinct(taxonConceptID))
+
+
+## RESTRICT TAXONOMIC SCOPE
+# restrict to taxa recognised by AFD
+species_df <- species_df[
+  grepl("^urn:lsid.biodiversity.org.au", species_df$taxonConceptID), ]
+  
+# import GRISS species to remove them from consideration
+griis <- read_csv("GRIIS_Introduced_Species.csv")
+# and selected other introduced species  
+other_introduced <- c(
+  house_mouse = "urn:lsid:biodiversity.org.au:afd.taxon:107696b5-063c-4c09-a015-6edfdb6f4d52",
+  cottoon_bollworm = "urn:lsid:biodiversity.org.au:afd.taxon:643c69f3-d060-4f65-a48a-e8d8466aa60b",
+  woodlouse = "urn:lsid:biodiversity.org.au:afd.taxon:4f5be6a5-41c0-4b49-ba7f-e4d693bd01a5",
+  cupboard_spider = "urn:lsid:biodiversity.org.au:afd.taxon:2b7b9500-61e1-43de-8064-3a609d0bf67f",
+  three_lined_potato_beetle = "urn:lsid:biodiversity.org.au:afd.taxon:a9970e29-da21-4ac7-b9ac-dbc92b35cce0" 
+)
+all_intro <- c(griis$guid[!is.na(griis$guid)], other_introduced)
+species_df <- species_df[!(species_df$taxonConceptID %in% all_intro), ]
+
+
+## REMOVE ULTRA-RARE SPECIES
 # split by taxonConceptID and keep species that:
   # occur in >1 electorates and
   # have been observed twice or more in total and
@@ -95,66 +123,56 @@ species_df <- do.call(rbind, species_list)
   # have a range > 0
 species_split <- split(species_df, species_df$taxonConceptID)
 species_split <- species_split[unlist(lapply(species_split, function(a){
-  nrow(a) > 1 & 
+  nrow(a) > 2 & 
   sum(a$count) > 2 & 
   max(a$count) > 10 & 
   (max(a$count) - min(a$count)) > 0
 }))]
 species_df <- do.call(rbind, species_split)
 
+
+## CALCULATE WEIGHTS
 # for each electorate, calculate the proportion of observations that each species occupies
 electorate_sums <- species_df |> 
   group_by(electorate) |> 
   summarize(electorate_sum = sum(count))
-species_df <- left_join(species_df, electorate_sums, by = "electorate")
-species_df$prop_by_electorate <- species_df$count / species_df$electorate_sum
+electorate_df$electorate_sum <- electorate_sums$electorate_sum
+species_df <- left_join(species_df, electorate_df, by = "electorate")
+
 
 # test that these proportions sum to one per electorate
 # species_df |> group_by(electorate) |> summarize(test = sum(prop_by_electorate))
 
 # calculate z score on electorate-level proportions
-species_df <- do.call(rbind, lapply(
+z_score <- function(x){((x - mean(x)) / sd(x))}
+
+species_df$z_species <- unlist(lapply(
   split(species_df, species_df$taxonConceptID),
-  function(a){
-    x <- a$prop_by_electorate
-    a$z_score_proportions <- (x - mean(x)) / sd(x)
-    y <- log(a$count)
-    a$z_score_counts <-  (y - mean(y)) / sd(y)
-    a$z_squared <- a$z_score_proportions * a$z_score_counts
-    a
-  }))
-  
-  ## OLD code  
-  # total <- sum(a$count)
-  # a$prop_species_obs <- (a$count / total) 
-  # a$prop_obs <- (a$prop_species_obs - min(a$prop_species_obs)) * total
-  # - 1/sum(a$count) ? would down-weight rare taxa
+  function(a){z_score(log(a$count))}))
 
+species_df$z_electorate <- unlist(lapply(
+  split(species_df, species_df$electorate),
+  function(a){z_score(log(a$count))}))
 
-# # investigate
-# ggplot(filter(species_df, electorate == "Lyons"),
-#   aes(x = log10(count), y = z_score_proportions)) +
-#   geom_point()
+names(species_df$z_species) <- NULL
+names(species_df$z_electorate) <- NULL
 
-
-# import GRISS species to remove them from consideration
-griis <- read_csv("GRIIS_Introduced_Species.csv")
-species_df <- species_df[!(species_df$taxonConceptID %in% griis$guid), ]
-
+species_df$z_sum <- species_df$z_species + species_df$z_electorate 
 
 # function to get taxon names
 join_spp <- function(df){
   df <- df[df$count > 10, ]
-  x <- search_identifiers(df$taxonConceptID[1:20]) |> as_tibble()
-  if(!any(x$rank == "species")){
-    x <- search_identifiers(df$taxonConceptID[1:50]) |> as_tibble()
-  }
+  nrow_vec <- if(nrow(df) < 100){seq_len(nrow(df))}else{seq_len(100)}
+  x <- search_identifiers(df$taxonConceptID[nrow_vec]) |> as_tibble()
+  # if(!any(x$rank == "species" & x$phylum == "Chordata")){
+  #   x <- search_identifiers(df$taxonConceptID[1:100]) |> as_tibble()
+  # }
   if(!any(colnames(x) == "vernacular_name")){
     x$vernacular_name <- NA
   }
   x <- cbind(
     df[seq_len(nrow(x)), ], 
-    x |> select(rank, phylum, class, order, family, species, vernacular_name)) |> filter(rank == "species")
+    x |> select(rank, phylum, class, order, family, species, vernacular_name)) |> filter(rank == "species" & phylum == "Chordata")
   x
 }
 
@@ -162,7 +180,7 @@ join_spp <- function(df){
 electorate_list <- lapply(
   split(species_df, species_df$electorate),
   function(a){
-    a <- a[order(a$z_squared, decreasing = TRUE), ] 
+    a <- a[order(a$z_sum, decreasing = TRUE), ] 
     join_spp(a)
   })
 
@@ -171,7 +189,7 @@ electorate_list <- lapply(
 # choose top-ranked species per electorate
 electorate_df <- do.call(rbind, lapply(electorate_list, function(a){a[1, ]})) 
 
-# electorate_df[, -1]
+
 
 
 # # Note that there is a duplicate species here: (now obsolete)
@@ -184,21 +202,25 @@ electorate_df <- do.call(rbind, lapply(electorate_list, function(a){a[1, ]}))
 # then choose next on list for second-placed electorate
 xt <- xtabs(~electorate_df$species)
 duplicated_names <- names(xt[xt > 1])
+electorate_df <- electorate_df[order(electorate_df$z_sum, decreasing = TRUE), ]
 for(i in seq_along(duplicated_names)){
    duplicated_rows <- which(
      electorate_df$species == duplicated_names[i])
    x <- electorate_df[duplicated_rows, ]
-   replace_row <- which.min(x$z_squared)
+   replace_row <- which.min(x$z_sum)
    electorate_tr <- x$electorate[replace_row]
    electorate_options <- electorate_list[[electorate_tr]]
    available_rows <- which(
-     !(electorate_options$species[-1] %in% electorate_df$species))
+     !(electorate_options$species[-1] %in% electorate_df$species)) # note: 
+      # a better option might be to replace present species if the 
+      # replacement in quesiton has a higher z_sum than that already in the dataset
    chosen_row <- available_rows[1] + 1 
    electorate_df[duplicated_rows[replace_row], ] <- electorate_options[chosen_row, ]
 }
 
 ## check any duplicates remaining
 # any(xtabs(~electorate_df$species) > 1)
+# electorate_df[, -1]
 
 ## look at groupings
 # str(electorate_df)
@@ -207,7 +229,7 @@ for(i in seq_along(duplicated_names)){
 
 # add in states
 electorate_df <- left_join(electorate_df, electorate_by_state, by = "electorate")
-
+electorate_df <- electorate_df[order(electorate_df$electorate), ]
 write.csv(electorate_df, "species_by_electorate.csv", row.names = FALSE)
 saveRDS(electorate_df, "species.rds")
 
@@ -215,12 +237,19 @@ saveRDS(electorate_df, "species.rds")
 ## MAPPING
 
 # reimport common names
-species_common <- read.csv("species_by_electorate_commonNames_31-03.csv")
-species_common <- species_common[, colnames(species_common)!="state"]
+# species_common <- read.csv("species_by_electorate_commonNames_31-03.csv")
+species_common <- electorate_df # temporary solution until images available
 
 # convert to spdf object
 elect <- boundaries_raw |>
-  left_join(species_common, by = c("Elect_div" = "electorate"))
+  left_join(
+    species_common[, c("electorate", "species", "vernacular_name")], 
+    by = c("Elect_div" = "electorate"))
+
+elect$label <-  paste0(
+  elect$Elect_div, ", ", elect$state, "<br>",
+  elect$vernacular_name
+)
   
 spdf_elect <- elect |> 
   st_zm() |> 
@@ -228,8 +257,38 @@ spdf_elect <- elect |>
 
 saveRDS(spdf_elect, "./2022_electorate_map/spatial_data.rds")
 
-# try simplifying
-# doesn't seem to noticeably reduce loading time - simplify further?
+
+leaflet_map <- leaflet() |> 
+  fitBounds(112, -45, 154, -10) |>
+  addProviderTiles(providers$CartoDB.Positron) |>  
+  addPolygons(
+    data = spdf_elect,
+    layerId = spdf_elect$Elect_div,
+    fillColor = "white",
+    fillOpacity = .2, 
+    color = "#111111", 
+    weight = 1, 
+    stroke = TRUE,
+    highlightOptions = highlightOptions(
+      color = "#C44D34", 
+      fillColor = "#C44D34", 
+      fillOpacity = 0.6,
+      weight = 3,
+      bringToFront = TRUE),
+    label = lapply(elect$label, HTML),
+    labelOptions = labelOptions(
+      style = list("font-weight" = "normal", 
+                   padding = "3px 5px"),
+      textsize = "12px",
+      direction = "auto")
+  )
+
+# saveRDS(leaflet_map, "2022_electorate_map/leaflet_map.rds")
+saveRDS(as_tibble(species_common), "2022_electorate_map/common_names.rds")
+
+
+# # try simplifying
+# # doesn't seem to noticeably reduce loading time - simplify further?
 spdf_elect_simpl <- rmapshaper::ms_simplify(spdf_elect, keep = 0.01)
 saveRDS(spdf_elect_simpl, "./2022_electorate_map/simpl_spatial_data.rds")
   
