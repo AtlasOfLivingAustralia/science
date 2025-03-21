70ca7116a85b5b872753a6cef4e93eaffbc39424
Dax Kellie <dax.kellie@csiro.au>
2025-03-21 14:57:57 +1100

    Update dataviz to group by bioregions

--- comms/2025-03-18_forests-day/tree-counts.R
+++ comms/2025-03-18_forests-day/tree-counts.R
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------------ #
-# Title: Eucalypts in Australia protected areas (for International Forest Day)
+# Title: Eucalypts in Australia's bioregions (for International Forest Day)
 # Author: Dax Kellie
-# Date: 18 March 2025
+# Date: 21 March 2025
 # ------------------------------------------------------------------ #
 
 library(galah)
@@ -10,81 +10,68 @@ library(here)
 library(sf)
 library(ggplot2)
 library(purrr)
+library(tidyr)
 
 galah_config(email = "dax.kellie@csiro.au")
 
-# search_all(fields, "capad")
-# search_all(fields, "cl11033") |>
-#   show_values()
+# search_all(fields, "ibra")
 
 # Eucalyptus observations
 counts <- galah_call() |>
   identify("eucalyptus") |>
   filter(year > 1999) |>
   apply_profile(ALA) |>
-  group_by(cl111033) |>
+  group_by(cl1048) |>
   atlas_counts()
 
 # Extract protected area names
-capad_names <- search_all(fields, "cl111033") |>
+ibra_names <- search_all(fields, "cl1048") |>
   show_values() |>
-  pull(cl111033)
+  pull(cl1048)
 
 # Retrieve species count by protected area and bind rows
-get_counts <- function(capad_names){
+get_counts <- function(ibra_names){
   
   # get counts
   result <- galah_call() |>
     identify("Eucalyptus") |>
     filter(year > 1999,
-           cl111033 == {capad_names}) |>
+           cl1048 == {ibra_names}) |>
     apply_profile(ALA) |>
     atlas_species() |>
     count()
   
   # light formatting to catch errors
-  tibble(name = {capad_names}, n_species = result |> pull("n"))
+  tibble(name = {ibra_names}, n_species = result |> pull("n"))
 }
 
-n_species <- map(capad_names, get_counts) |>
+n_species <- map(ibra_names, get_counts) |>
   bind_rows()
 
 # join eucalyptus counts with species number counts
 counts_joined <- counts |>
-  left_join(n_species, join_by(cl111033 == name))
+  left_join(n_species, join_by(cl1048 == name))
 
 
 
 # Add label, specify Indigenous Protected Areas for plotting later
-labels <- counts_joined |>
+counts_extra <- counts_joined |>
   arrange(desc(n_species)) |>
   mutate(
-    label = case_when(
-      n_species > 500 ~ glue::glue("{scales::comma(n_species)} species recorded"),
+    n_row = row_number(),
+    top_species = case_when(
+      n_species > 160 ~ as.character(n_row),
       .default = NA_character_
     ),
-    ipa = case_when(
-    cl111033 == "Indigenous Protected Area" ~ "IPA",
-    cl111033 == "Aboriginal Area" ~ "IPA",
-    .default = "not"
+    state = case_when(
+      cl1048 == "South Eastern Highlands" ~ "NSW/VIC",
+      cl1048 == "Sydney Basin" ~ "NSW",
+      cl1048 == "Mallee" ~ "WA",
+      .default = NA_character_
     )
   )
 
-# species count in Indigenous Protected Areas
-ipa_species <- labels |>
-  filter(ipa == "IPA") |>
-  summarise(total = sum(n_species))
-
-# ipa_obs <- labels |>
-#   filter(ipa == "IPA") |>
-#   summarise(total = sum(count)) |>
-#   pull(total)
-
-# handy-dandy position/size filter
-counts_joined |>
-  arrange(cl111033) |>
-  mutate(n_row = row_number()) |>
-  filter(cl111033 == "Indigenous Protected Area")
+counts_extra
 
 ## PLOT
 
@@ -93,93 +80,90 @@ credits <-
   glue::glue("
        **Dataviz by Dax Kellie** <br><br>
        Data consists of observations of genus *Eucalyptus* <br><br>
-       Protected area types are from the Collaborative Australian Protected Areas Database<br>
+       *Interim Biogeographic Regionalisation for Australia (IBRA) terrestrial bioregions<br>
        Data downloaded using the galah package <br>
        
        ") |> paste0()
+
 
 library(showtext)
 font_add_google("Roboto", "roboto")
 showtext_auto()
 
 ggplot() +
-  geom_segment(data = labels,
-               aes(x = cl111033,
-                   xend = cl111033,
+  geom_segment(data = counts_extra,
+               aes(x = cl1048,
+                   xend = cl1048,
                    y = 0,
                    yend = count),
                colour = "#3e1f0d",
                alpha = 0.8) + 
-  geom_point(data = labels,
-             aes(x = cl111033,
+  geom_point(data = counts_extra,
+             aes(x = cl1048,
                  y = count,
                  size = n_species,
-                 fill = ipa),
+                 # fill = ipa
+                 ),
+             fill = "#707b5b",
              shape = 21,
              alpha = 0.7,
              # fill = "#707b5b",
              colour = "NA") + 
-  scale_fill_manual(
-    values = c("#853b25", "#707b5b")
-    ) +
+  geom_point(data = counts_extra |> drop_na(top_species),
+             aes(x = cl1048,
+                 y = count,
+                 size = n_species,
+                 colour = cl1048),
+             shape = 21,
+             fill = "transparent",
+             colour = "#4f4f4f",
+             stroke = 1
+             ) +
   scale_y_log10(limits = c(1, 1e7), labels = scales::comma_format()) +
   scale_x_discrete(expand = c(-1.05, 0)) +
-  # geom_point(aes(x = 25,
-  #                y = 103338,
-  #                size = 520),
-  #            shape = 21,
-  #            stroke = 1.2,
-  #            fill = NA,
-  #            colour = "#3A442A",
-  #            alpha = 0.7
-  #            ) +
-  # annotations
-  ggrepel::geom_text_repel(aes(label = glue::glue("
-                                                  {ipa_species} species
-                                                  in all 
-                                                  Indigenous
-                                                  Protected 
-                                                  Areas
-                                                  "),
-                               x = 25,
-                               y = 2162,
-                               colour = "#853b25",
-                               colour = after_scale(colorspace::darken(colour, .6))),
-                           nudge_x = c(-5.6),
-                           nudge_y = c(1.3),
-                           segment.size = 0,
-                           segment.curvature = c(0.001),
-                           family = "roboto",
-                           lineheight = 0.9
-                           ) +
-  annotate("text",
-           x = 25, y = 4000000, label = "526 species recorded in National Parks",
-           colour = "#3A442A",
-           family = "roboto") +
+  ggrepel::geom_text_repel(
+    data = counts_extra |> drop_na(top_species),
+    aes(
+      x = cl1048,
+      y = count,
+      label =
+        glue::glue("
+                 #{top_species} most species
+                 {cl1048}, {state} | {n_species} sp.
+                 ")
+    ),
+    colour = "#4f4f4f",
+    nudge_x = c(-1.6),
+    nudge_y = c(1.3),
+    segment.size = 0.1,
+    segment.curvature = c(0.001),
+    family = "roboto",
+    lineheight = 0.9
+  ) +
   # legend
-  geom_point(aes(x = 52,
-                 y = 5000000,
+  geom_point(aes(x = 2,
+                 y = 6000000,
                  size = 10),
              colour = "#707b5b",
              alpha = 0.7) +
   annotate("text",
-           x = 55, y = 5000000, label = "-- 10 species",
+           x = 8, y = 6000000, label = "-- 10 species",
            colour = "#3A442A",
            family = "roboto") +
-  geom_point(aes(x = 52,
-                 y = 700000,
+  geom_point(aes(x = 2,
+                 y = 1000000,
                  size = 100),
              colour = "#707b5b",
              alpha = 0.7) +
   annotate("text",
-           x = 56, y = 700000, label = "-- 100 species",
+           x = 8, y = 1000000, label = "-- 100 species",
            colour = "#3A442A",
            family = "roboto") +
-  scale_size(range = c(1, 40)) + # scale points - IMPORTANT
+  scale_size(range = c(1, 32)) + # scale points - IMPORTANT
   labs(y = "Number of Observations",
-       x = "Protected Area",
-       title = "Eucalypts in Australia's protected areas",
-       subtitle = "Observations and unique species of *Eucalyptus* in different types of protected areas (CAPAD) since 2000",
+       x = "IBRA* Bioregion",
+       title = "Eucalypts in Australia's bioregions",
+       subtitle = "Observations and unique species of *Eucalyptus* in IBRA Bioregions since 2000",
        caption = credits) +
   pilot::theme_pilot(grid = "h",
                      axes = "b") +
@@ -208,6 +192,6 @@ ggplot() +
 # save
 showtext_opts(dpi = 320)
 ggsave(here::here("comms", "2025-03-18_forests-day", "plots", "eucalyptus-trees.png"),
-       height = 11, width = 14, unit = "in", dpi = 320)
+       height = 11, width = 15.3, unit = "in", dpi = 320)
 
 
