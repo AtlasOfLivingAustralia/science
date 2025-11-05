565c9a5c852dfb26a541ea5371e4999da12067d4
Dax Kellie <dax.kellie@csiro.au>
2025-11-05 16:44:48 +1100

    Download all data in specified regions

--- projects/2025-10-20_nsw-biodiversity-trust/scripts/download-data-better.R
+++ projects/2025-10-20_nsw-biodiversity-trust/scripts/download-data-better.R
@@ -59,14 +59,88 @@ result <- galah_call() |>
   select(
     group = "basic",
     dataResourceName, 
-    dataResourceUid
-    # cl1049, # IBRA subregions
+    dataResourceUid,
+    basisOfRecord,
+    cl1049 # IBRA subregions
     # cl1048  # IBRA regions
   ) |> 
-  atlas_occurrences(mint_doi = TRUE)
+  atlas_occurrences()
 
 result
 
 attributes(result)$doi
 # https://doi.org/10.26197/ala.2a3b0122-e225-4925-8c94-102f1640cb6f
 
+
+
+
+
+## All data (not just NatureMapr)
+
+# test query by returning counts
+galah_call() |>
+  filter(
+    species_list == "dr33163",
+    # dataResourceName == "NatureMapr",
+    cl1049 %in% selected_regions$sub_name_7
+  ) |>
+  group_by(cl1049) |>
+  atlas_counts()
+
+
+## Download
+
+get_records <- function(region) {
+  # Download data
+  result <- galah_call() |>
+    filter(
+      species_list == "dr33163",
+      cl1049 == {{region}}
+    ) |>
+    select(
+      group = "basic",
+      dataResourceName, 
+      dataResourceUid,
+      basisOfRecord,
+      cl1049 # IBRA subregions
+      # cl1048  # IBRA regions
+    ) |> 
+    atlas_occurrences(mint_doi = TRUE)
+  
+  result_with_doi <- result |>
+    mutate(
+      doi = attributes(result)$doi
+    )
+  
+  # print the doi for good measure
+  cli::cli_inform("{attributes(result)$doi}")
+  
+  return(result_with_doi)
+}
+
+# Download data
+# download occurrence records & dois
+
+regions_list <- as.list(selected_regions$sub_name_7)
+
+tictoc::tic()
+occs <- map(regions_list, get_records)
+
+# merge
+occs_combined <- occs |>
+  bind_rows()
+tictoc::toc(); beepr::beep(2)
+
+# see dois that make up data
+dois <- occs_combined |>
+  distinct(cl1049, doi)
+
+dois
+
+# save
+write.csv(dois,
+          file = here("projects",
+                      "2025-10-20_nsw-biodiversity-trust",
+                      "data",
+                      "downloaded-data",
+                      "2025-10-28_doi-list_all-records.csv"))
